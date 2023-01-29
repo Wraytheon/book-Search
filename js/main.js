5e50a15db4eb5f9ed9bf83179e5d225e4b235fd6
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-01-28 20:33:46 -0500

    removed carousel, images show after button click then disappear

--- js/main.js
+++ js/main.js
@@ -1,95 +1,35 @@
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 /* Loads local storage on page load and place it in DOM*/
-document.querySelector('p').innerText = localStorage.getItem('bookTitle');
-/*Loads locally stored cover images on page load and place them in DOM*/
-document.querySelector('img').src = localStorage.getItem('coverImage');
+document.querySelector("p").innerText = localStorage.getItem("books");
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://openlibrary.org/isbn/${choice}.json`
+function getFetch() {
+  const choice = document.querySelector("input").value;
+  const url = `https://openlibrary.org/isbn/${choice}.json`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data.title)
-        /*If no bookTitle have inputted yet, the first local storage value will be 'null' so this replaces 'null' with the first input*/
-        if(!localStorage.getItem('bookTitle')) {
-            /*Store book titles*/
-            localStorage.setItem('bookTitle', data.title)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
 
-            /*Store cover images*/
-            const imageUrlString = JSON.stringify(images)
+      /*Retrieve cover image*/
+      const cover = `https://covers.openlibrary.org/b/id/${data.covers}-S.jpg`
+      /*Show cover image */
+      document.querySelector('img').src = cover;
+      /*If no books have inputted yet, the first local storage value will be 'null' so this replaces 'null' with the first input*/
+      if (!localStorage.getItem("books")) {
+        localStorage.setItem("books", data.title);
 
-            localStorage.setItem('coverImage', imageUrlString)
+        console.log(data.title);
+      } else {
+        /*When button is clicked, everything that is already stored in the local storage under keyname 'books' as string concatenated with the title just inputted: Example: Bible ; Atomic Habits*/
+        let titles = localStorage.getItem("books") + " ; " + data.title;
 
-
-        } else {
-            /*When button is clicked, everything that is already stored in the local storage under keyname 'bookTitle' as string concatenated with the title just inputted: Example: Bible ; Atomic Habits*/
-            let title = localStorage.getItem('bookTitle') + " ; " + data.title
-
-            /*Create local storage holder that stores entire concatenated string of inputted bookTitle*/
-            localStorage.setItem('bookTitle', title)
-
-            // let covers = localStorage.getItem('
-
-            /*Display carousel */
-            document.getElementById('carousel').style.display = 'initial';
-        }
-        // /*Retrieve titles from localStorage and place in DOM*/
-        document.querySelector('p').innerText = localStorage.getItem('bookTitle');
-
-        /*Display book cover*/
-        console.log(data.covers)
-        /*Plug the id retrieved from data.covers into the cover images template url*/
-        const coverUrl = `https://covers.openlibrary.org/b/id/${data.covers}-S.jpg`
-        console.log(coverUrl);
-        /*Show the cover*/
-        document.querySelector('img').src = coverUrl;
-
-        /* Carousel */
-        const carousel = document.getElementById("carousel");
-        const currentImage = document.getElementById("current-image");
-        const prevBtn = document.getElementById("prev-btn");
-        const nextBtn = document.getElementById("next-btn");
-
-        // List of image URLs
-        const images = [];
-
-        data.forEach(image => {
-            images.push(image.covers);
-          });
-
-        /* Starting index for image urls array*/
-        let currentIndex = 0;
-
-        // Update the current image
-        function updateCurrentImage() {
-        currentImage.src = images[currentIndex];
-        }
-
-        // Show the previous image
-        prevBtn.addEventListener("click", function() {
-        currentIndex--;
-        if (currentIndex < 0) {
-            currentIndex = images.length - 1;
-        }
-        updateCurrentImage();
-        });
-
-        // Show the next image
-        nextBtn.addEventListener("click", function() {
-        currentIndex++;
-        if (currentIndex >= images.length) {
-            currentIndex = 0;
-        }
-        updateCurrentImage();
-        });
-
-        // Initialize the carousel
-        updateCurrentImage();
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+        /*Create local storage holder that stores entire concatenated string of inputted books*/
+        localStorage.setItem("books", titles);
+      }
+      // /*Retrieve titles from localStorage and place in DOM*/
+      document.querySelector("p").innerText = localStorage.getItem("books");
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
-
