736d96a20238a67576770dc78da214718461d7ec
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-01-30 15:33:21 -0500

    no local storage usage, basic search functionality complete

--- js/main.js
+++ js/main.js
@@ -1,39 +1,37 @@
 document.querySelector("button").addEventListener("click", getFetch);
-/* Loads local storage on page load and place it in DOM*/
-document.querySelector("p").innerText = localStorage.getItem("books");
 /*Hide image on page load*/
-document.querySelector("img").src = localStorage.getItem("bookCover")
+document.querySelector("img").style.display = 'none'
 
 
 function getFetch() {
   const isbn = document.querySelector("input").value;
+  console.log(isbn)
   const url = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`;
 
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
+    /* Variable holding the prefix for accessing the data*/
+      const dataPrefix = data[`ISBN:${isbn}`]
 
-      /*Retrieve cover image*/
-      const cover = `https://covers.openlibrary.org/b/id/${data.covers}-S.jpg`
-      /*Show cover image */
-      document.querySelector('img').src = cover;
-      /*If no books have inputted yet, the first local storage value will be 'null' so this replaces 'null' with the first input*/
-      if (!localStorage.getItem("books")) {
-        localStorage.setItem("books", data.title);
+      /*Title data retrieval and display*/
+      const title = dataPrefix.title
+      document.querySelector("h2").innerText = title
+      console.log(title)
+      /*Isbn data display*/
+      document.querySelector("h3").innerText = isbn
 
-        console.log(data.title);
-      } else {
-        /*When button is clicked, everything that is already stored in the local storage under keyname 'books' as string concatenated with the title just inputted: Example: Bible ; Atomic Habits*/
-        let titles = localStorage.getItem("books") + " ; " + data.title;
+      /*Author data retrieval and display*/
+      const author = dataPrefix.authors[0].name
+      document.querySelector('h4').innerText = author
+      console.log(author)
 
-        /*Create local storage holder that stores entire concatenated string of inputted books*/
-        localStorage.setItem("books", titles);
-
-        /* Show cover image */
-        document.querySelector('img').style.display = 'initial';
-      }
-      // /*Retrieve titles from localStorage and place in DOM*/
-      document.querySelector("p").innerText = localStorage.getItem("books");
+      /*Cover image retrieval and display*/
+      const coverImg = dataPrefix.cover.medium
+      console.log(coverImg)
+      document.querySelector('img').src = coverImg;
+      /*Display image*/
+      document.querySelector("img").style.display = "initial"
     })
     .catch((err) => {
       console.log(`error ${err}`);
