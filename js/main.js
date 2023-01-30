23f3215923ae814a9e7189fb69b2cd414117efe4
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-01-30 13:43:42 -0500

    trying to find way to show last book image on refresh but hide on first page load

--- js/main.js
+++ js/main.js
@@ -1,39 +1,60 @@
 document.querySelector("button").addEventListener("click", getFetch);
-/* Loads local storage on page load and place it in DOM*/
-document.querySelector("p").innerText = localStorage.getItem("books");
+
+/* Loads last inputted title, author, and image on page load and place it in DOM*/
+document.querySelector("h2").innerText = localStorage.getItem("bookTitle");
+
+document.querySelector("h3").innerText = localStorage.getItem("bookAuthor");
+
 /*Hide image on page load*/
-document.querySelector("img").style.display = 'none';
+document.querySelector("img").src = localStorage.getItem("bookCover")
 
 
 function getFetch() {
-  const choice = document.querySelector("input").value;
-  const url = `https://openlibrary.org/isbn/${choice}.json`;
+  const isbn = document.querySelector("input").value;
+  const url = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`;
 
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
 
-      /*Retrieve cover image*/
-      const cover = `https://covers.openlibrary.org/b/id/${data.covers}-S.jpg`
+      const dataPrefix = data[`ISBN:${isbn}`]
+
+      const title = dataPrefix.title
+
+      const author = dataPrefix.authors[0].name
+      document.querySelector('h3').innerText = author
+      console.log(author)
+
+      const coverImg = dataPrefix.cover.medium
+      console.log(coverImg)
+      
       /*Show cover image */
-      document.querySelector('img').src = cover;
+      document.querySelector('img').src = coverImg;
+      document.querySelector('img').style.display = "initial"
+
+      console.log(dataPrefix.title)
+      console.log(isbn)
       /*If no books have inputted yet, the first local storage value will be 'null' so this replaces 'null' with the first input*/
-      if (!localStorage.getItem("books")) {
-        localStorage.setItem("books", data.title);
+      if (!localStorage.getItem("bookTitle")) {
+        localStorage.setItem("bookTitle", title);
+        localStorage.setItem("bookAuthor", author);
+        localStorage.setItem("bookImg", coverImg);
 
-        console.log(data.title);
+        console.log(title);
       } else {
-        /*When button is clicked, everything that is already stored in the local storage under keyname 'books' as string concatenated with the title just inputted: Example: Bible ; Atomic Habits*/
-        let titles = localStorage.getItem("books") + " ; " + data.title;
 
-        /*Create local storage holder that stores entire concatenated string of inputted books*/
-        localStorage.setItem("books", titles);
 
         /* Show cover image */
         document.querySelector('img').style.display = 'initial';
       }
+
+      localStorage.getItem("bookTitle")
+      localStorage.getItem("bookAuthor")
+
       // /*Retrieve titles from localStorage and place in DOM*/
-      document.querySelector("p").innerText = localStorage.getItem("books");
+      document.querySelector("h2").innerText = title;
+    //   const author = `https://openlibrary.org${data.authors[0]}.json`
+    //   console.log(author)
     })
     .catch((err) => {
       console.log(`error ${err}`);
